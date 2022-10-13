ef42121fb2f563902b48e51d07c9343a6704f8ae
adfwer233 <ba123698745@126.com>
2022-10-13 11:34:43 +0800

    fix: simple editor lsp connection

--- frontend/src/components/MonacoEditorPanel/SimpleEditor.vue
+++ frontend/src/components/MonacoEditorPanel/SimpleEditor.vue
@@ -9,8 +9,6 @@ import { onMounted, ref, shallowRef } from "vue";
 import * as monaco from "monaco-editor";
 import {
   MonacoLanguageClient,
-  CloseAction,
-  ErrorAction,
   MonacoServices,
   MessageTransports,
 } from "monaco-languageclient";
@@ -30,34 +28,37 @@ const options = {
   language: "python",
   automaticLayout: true,
   bracketPairColorization: true,
-  inDiffEditor: true,
+  model: monaco.editor.createModel("import os", "python"),
 };
+
+function createLanguageClient(
+  transports: MessageTransports
+): MonacoLanguageClient {
+  return new MonacoLanguageClient({
+    name: "Python Language Client",
+    clientOptions: {
+      documentSelector: ["python"],
+    },
+    connectionProvider: {
+      get: () => {
+        return Promise.resolve(transports);
+      },
+    },
+  });
+}
 
 onMounted(() => {
   editor.value = monaco.editor.create(monacoEditor.value, options);
 
   MonacoServices.install();
 
-  const webSocket = new WebSocket("ws://localhost:30005");
+  const webSocket = new WebSocket("ws://localhost:30000");
   // define the connection(websocket) to the language server
   webSocket.onopen = () => {
     const socket = toSocket(webSocket);
-    const reader = WebSocketMessageReader(socket);
-    const writer = WebSocketMessageWriter(socket);
-    const languageClient = new MonacoLanguageClient({
-      clientOptions: {
-        documentSelector: ["python"],
-        errorHandler: {
-          error: () => ({ action: ErrorAction.Continue }),
-          closed: () => ({ action: CloseAction.DoNotRestart }),
-        },
-      },
-      connectionProvider: {
-        get: () => {
-          return Promise.resolve(MessageTransports({ reader, writer }));
-        },
-      },
-    });
+    const reader = new WebSocketMessageReader(socket);
+    const writer = new WebSocketMessageWriter(socket);
+    const languageClient = createLanguageClient({ reader, writer });
     languageClient.start();
     reader.onClose(() => languageClient.stop());
     console.log("websocket on open");
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: "@/components",
+      },
+      fallback: {
+        path: false,
+      },
+    },
+    plugins: [
+      new MonacoWebpackPlugin({
+        languages: ["python"],
+      }),
+    ],
+  },
 });
