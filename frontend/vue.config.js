2840a9f45ca42a7751cf90a027023be86009081a
qgk25 <qg3035276228@126.com>
2022-10-21 21:30:08 +0800

    feat: add basic tab

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
