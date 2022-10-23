af0719f2e7adfe57ef90d682f9f08bc5aa7e0c22
qgk25 <qg3035276228@126.com>
2022-10-23 23:56:32 +0800

    fix: finish merge and can run successfully

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,12 +4,6 @@ import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-<<<<<<< HEAD
-createApp(App)
-  .use(store)
-  .use(router)
-  .use(ElementPlus, { size: "small", zIndex: 3000 })
-=======
 import ArcoVue from "@arco-design/web-vue";
 import "@arco-design/web-vue/dist/arco.css";
 
@@ -18,5 +12,4 @@ createApp(App)
   .use(router)
   .use(ElementPlus)
   .use(ArcoVue)
->>>>>>> main
   .mount("#app");
