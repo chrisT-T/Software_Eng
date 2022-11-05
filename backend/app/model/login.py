f3b65164baf5ffbd5ad78eda5c74e79d74b91a5a
adfwer233 <ba123698745@126.com>
2022-11-05 13:37:59 +0800

    feat: add n * m relationships

--- backend/app/model/login.py
+++ backend/app/model/login.py
@@ -2,14 +2,22 @@ from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app.extensions import db, login_manager
+from app.model.relation_tables import (admin_table, editable_table,
+                                       pending_table, readonly_table)
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'tbl_user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(64))
+    email = db.Column(db.String(64))
     password_hash = db.Column(db.String(128))
+    
     created_projects = db.relationship("Project", back_populates="creator")
+    readonly_projects = db.relationship("Project", secondary=readonly_table, back_populates="readonly_users")
+    editable_projects = db.relationship("Project", secondary=editable_table, back_populates="editable_users")
+    admin_projects = db.relationship("Project", secondary=admin_table, back_populates="admin_users")
+    pending_projects = db.relationship("Project", secondary=pending_table, back_populates="pending_users")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
--- backend/test/test_database.py
+++ backend/test/test_database.py
@@ -18,12 +18,18 @@ class DataBaseTestCase(unittest.TestCase):
         self.app_context.push()
         db.drop_all()
         db.create_all()
-        me = User(id=0, username="test", password_hash=generate_password_hash("test"))
+        me = User(id=0, username="test1", password_hash=generate_password_hash("test1"))
+        user1 = User(id=1, username="test2", password_hash=generate_password_hash("test2"))
         project1 = Project(project_name="proj", project_language="python", creator_id=0)
         project2 = Project(project_name="proj2", project_language="python", creator_id=0)
+        project3 = Project(project_name="proj3", project_language="python", creator_id=0)
+
+        me.readonly_projects.append(project1)
+        me.readonly_projects.append(project2)
+
         db.session.add(me)
-        db.session.add(project1)
-        db.session.add(project2)
+        db.session.add(user1)
+        db.session.add_all([project1, project2, project3])
         db.session.commit()
 
         db.session.commit()
@@ -33,8 +39,15 @@ class DataBaseTestCase(unittest.TestCase):
         db.drop_all()
 
         self.app_context.pop()
-    
+
     def test_relation(self):
         proj_list = Project.query.filter_by(creator_id=0).all()
         print(proj_list)
-        self.assertEqual(len(proj_list), 2)
+        self.assertEqual(len(proj_list), 3)
+
+        me = User.query.filter_by(id=0).first()
+        print(me.readonly_projects)
+        self.assertEqual(len(me.readonly_projects), 2)
+
+        user1 = User.query.filter_by(id=1).first()
+        self.assertEqual(len(user1.readonly_projects), 2)
